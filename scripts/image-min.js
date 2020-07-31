875bd896bf842fada936d3e8adb37d46b74e590a
Méril <meril.pilon@gmail.com>
2020-07-31 12:40:44 +0200

    chore(build): resize images if width > 1920px or height > 1440px (#75)

--- scripts/image-min.js
+++ scripts/image-min.js
@@ -1,30 +1,85 @@
 const util = require("util")
 const path = require("path")
 
+const glob = require("glob")
 const fs = require("graceful-fs")
 const imagemin = require("imagemin")
 const imageminGifsicle = require("imagemin-gifsicle")
-const imageminMozJpeg = require("imagemin-mozjpeg")
-const imageminPngquant = require("imagemin-pngquant")
 const imageminSvgo = require("imagemin-svgo")
 const makeDir = require("make-dir")
+const sharp = require("sharp")
 
 const writeFile = util.promisify(fs.writeFile)
 const src = "static/img"
 const dist = src
 
-if (process.env.NETLIFY === "true") {
-  imagemin([`${src}/**/*.{gif,svg,jpg,jpeg,png}`], {
+const run = async () => {
+  const doByExt = async (ext) => {
+    let successList = []
+    const files = glob.sync(`${src}/**/*.${ext}`)
+
+    console.log(`### sharp() for .${ext} images ###\n`)
+
+    for (const file of files) {
+      await sharp(file)
+        .resize({
+          height: 1440,
+          fit: sharp.fit.inside,
+          width: 1920,
+          withoutEnlargement: true,
+        })
+        .toFormat(ext === "jpg" ? "jpeg" : ext, {
+          progressive: true,
+          quality: 75,
+        })
+        .toFile(
+          file.replace(
+            new RegExp(`\/([\\w\\d-_]{0,255})\.${ext}$`),
+            `/_$1.${ext}`,
+          ),
+        )
+        .then(function () {
+          successList.push(file)
+          console.log(`Optimising:  ${file}`)
+        })
+        .catch(function (err) {
+          console.log(`Fail: ${file}`, "\n", err)
+        })
+    }
+
+    console.log("\n------------------------------")
+    console.log("TOTAL:", successList.length)
+
+    successList = []
+
+    console.log(`\n### rename() for .${ext} images ###\n`)
+
+    const rename = glob.sync(`${src}/**/_*.${ext}`)
+
+    for (const file of rename) {
+      fs.renameSync(
+        file,
+        file.replace(new RegExp(`\/_([\\w\\d]{0,255})\.${ext}$`), `/$1.${ext}`),
+      )
+      console.log(`Renaming:  ${file}`)
+      successList.push(file)
+    }
+
+    console.log("\n------------------------------")
+    console.log("TOTAL:", successList.length)
+    console.log("\n")
+  }
+
+  await doByExt("jpg")
+  await doByExt("png")
+
+  console.log(`### Optimise gif and svg images ###\n`)
+
+  imagemin([`${src}/**/*.{gif,svg}`], {
     plugins: [
       imageminGifsicle({
         interlaced: true,
         optimizationLevel: 2,
-      }),
-      imageminMozJpeg({
-        quality: 75,
-      }),
-      imageminPngquant({
-        quality: [0.65, 0.8],
       }),
       imageminSvgo({
         plugins: [
@@ -46,6 +101,7 @@ if (process.env.NETLIFY === "true") {
   }).then((files) =>
     files.forEach(async (file) => {
       let source = path.parse(file.sourcePath)
+      console.log(`Optimised:  ${file.sourcePath}`)
 
       file.destinationPath = `${source.dir.replace(src, dist)}/${source.name}${
         source.ext
@@ -55,4 +111,8 @@ if (process.env.NETLIFY === "true") {
       await writeFile(file.destinationPath, file.data)
     }),
   )
+}
+
+if (process.env.NETLIFY === "true" || process.env.FORCE === "true") {
+  run()
 }
